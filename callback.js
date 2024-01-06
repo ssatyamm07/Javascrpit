6bdfa45422b4643ce80d88fde1de5de627011bad
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2024-01-06 23:02:33 +0530

    learning

---
+++ callback.js
@@ -0,0 +1,17 @@
+function square(n) {
+    return n * n;
+}
+function cube(n) {
+    return n * n * n;
+}
+
+function sumOfSomthing(a,b, callback) {
+    console.log(a);
+    console.log(callback);
+    const val1 = callback(a);
+    const val2 = callback(b);
+    return val1 + val2;
+}
+
+const ans = sumOfSomthing(2,2, square);
+console.log(ans);
