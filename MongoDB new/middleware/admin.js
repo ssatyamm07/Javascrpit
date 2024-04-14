a0c82b0a4fffeb117429f669dbdc5cc02c2714a4
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2024-04-14 16:50:41 +0530

    middleare added

---
+++ MongoDB new/middleware/admin.js
@@ -0,0 +1,22 @@
+const {Admin} = require('../db');
+
+function adminMiddleware(req, res, next) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    Admin.findOne({
+        username: username,
+        password: password
+    })
+
+    .then(function(value) {
+        if (value) {
+            next();
+        } else {
+            res.status(403).json({
+                message: 'Unauthorized Access'
+            });
+        }   
+    })
+
+}    
+    
